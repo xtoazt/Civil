6834b545b87711dda6cd738d84f0fabd5f55dbbe
Quartinal <prehistorical.dinosaurs@outlook.com>
2024-04-21 17:44:54 -0700

    panic key fix

--- javascripts/cloak.js
+++ javascripts/cloak.js
@@ -4,33 +4,57 @@ const urlToOpen = document.querySelector('.container div .urlToOpen');
 panicKey.addEventListener('click', (e) => {
     e.preventDefault();
 
-    const p = prompt("Do you want these settings to last throughout multiple sessions [yes/no]");
+    panicKey.addEventListener('input', (e) => {
+        e.preventDefault();
 
-    if (p === 'yes') {
-        panicKey.addEventListener('input', (e) => {
+        panicKey.addEventListener('keypress', (e) => {
             e.preventDefault();
-        
-            panicKey.addEventListener('keypress', (e) => {
-                e.preventDefault();
-            
-                if (e.key === 'Enter') {
-                    const selectedKey = panicKey.value.trim() || ']';
-            
-                    localStorage.setItem('panicKey', selectedKey);
-        
-                    panicKey.addEventListener('keypress', (e) => {
-                        if (e.key === localStorage.getItem('panicKey')) {
-                            const url = urlToOpen.value;
 
-                            if (url == '' || !url.startsWith('https') || !url.startsWith('http')) {
-                                alert("Please provide a valid URL");
-                            }
+            if (e.key === 'Enter') {
+                const selectedKey = panicKey.value.trim() || ']';
 
-                            
+                sessionStorage.setItem('panicKey', selectedKey);
+
+                window.addEventListener('keypress', (e) => {
+                    if (e.key === sessionStorage.getItem('panicKey')) {
+                        const url = urlToOpen.value || 'https://myapps.classlink.com';
+
+                        if (url == '') {
+                            alert("Please provide a valid URL");
                         }
-                    });
-                }
-            });
+
+                        window.location.replace(url);
+
+                        if (!url.startsWith('https') || !url.startsWith('http') && url.match(/(https?:\/\/([-\w\.]+[-\w]){1,63}\.[a-zA-Z]{2,6}(\/[^\s]*)?)/g)) {
+                            window.location.replace(`https://${url}`);
+                        }
+                    }
+                });
+            }
         });
-    }
+
+        urlToOpen.addEventListener('keypress', (e) => {
+            if (e.key === 'Enter') {
+                const selectedKey = panicKey.value.trim() || ']';
+
+                sessionStorage.setItem('panicKey', selectedKey);
+
+                window.addEventListener('keypress', (e) => {
+                    if (e.key === sessionStorage.getItem('panicKey')) {
+                        const url = urlToOpen.value || 'https://myapps.classlink.com';
+
+                        if (url == '') {
+                            alert("Please provide a valid URL");
+                        }
+
+                        window.location.replace(url);
+
+                        if (!url.startsWith('https') || !url.startsWith('http') && url.match(/(https?:\/\/([-\w\.]+[-\w]){1,63}\.[a-zA-Z]{2,6}(\/[^\s]*)?)/g)) {
+                            window.location.replace(`https://${url}`);
+                        }
+                    }
+                });
+            }
+        });
+    });
 });
